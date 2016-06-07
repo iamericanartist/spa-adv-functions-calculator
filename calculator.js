a5d41b40274b3b47f7fa008e207f096e696e379d
Eric Denton <eric@iamericanartist.com>
2016-06-06 22:19:39 -0500

    commiting partial files for backup

---
+++ calculator.js
@@ -0,0 +1,101 @@
+
+// var addButtonEl = document.getElementById("addButton");             // Reference to the "+" button element in the DOM
+// var subtractButtonEl = document.getElementById("subtractButton");   // Reference to the "-" button element in the DOM
+// var multiplyButtonEl = document.getElementById("multiplyButton");   // Reference to the "*" button element in the DOM
+// var divideButtonEl = document.getElementById("divideButton");       // Reference to the "/" button element in the DOM
+// var buttonReset = document.getElementById("buttonReset");           // Reference to the button Reset element in the DOM
+// var resultsDiv = document.getElementById("results");
+
+
+
+// function additionFunction (input 1, input2) {
+//   var added = input1 + input2;
+//   console.log("input1 + input2 =", added );
+//   return added;
+// }
+// function subtractionFunction (input 1, input2) {
+//   var subtracted = input1 - input2;
+//   console.log("input1 - input2 =", subtracted);
+//   return subtracted;
+// }
+// function multiplicationFunction (input 1, input2) {
+//   var multiplied = input1 * input2;
+//   console.log("input1 * input2 =", multiplied);
+//   return multiplied;
+// }
+// function divisionFunction (input 1, input2) {
+//   var divided = input1 / input2;
+//   console.log("input1 / input2 =", divided);
+//   return divided;
+// }
+
+
+
+// function cleanUp() {
+//   console.log("Clear Clicker", event );
+//   document.getElementById("userInput").value = "";
+//   document.getElementById("userInput").value = "";
+  
+//   resultsDiv.innerHTML = "Your Conversion Results";
+// }
+
+// // addButtonEl.addEventListener("click", function (event) {
+// //   console.log("event",event);
+// // });
+
+// document.getElementById("addButton").addEventListener("click", function (event) {
+//   console.log("event",event);
+// });
+// subtractButtonEl.addEventListener("click", subtractionFunction);
+// multiplyButtonEl.addEventListener("click", );
+// divideButtonEl.addEventListener("click", );
+// buttonReset.addEventListener("click", cleanUp);
+document.getElementById("buttonReset").addEventListener("click", 
+  function (event) {
+    console.log("event",event);
+  }
+);
+
+document.getElementById("buttonReset").addEventListener("click", function(event) {
+    console.log("event", event);
+    // songDomEl.innerHTML += `${event.target.innerHTML}`
+  });
+
+console.log("asdfasdf");
+alert("hellooooooo!");
+// window.addEventListener("keypress", enterKeyPress); 
+
+/*
+  Create a function that multiplies two numbers
+  passed in as arguments. Return the product.
+ */
+
+
+/*
+  Create a function that adds two numbers
+  passed in as arguments. Return the sum.
+ */
+
+
+/*
+  Create a function that subtracts two numbers
+  passed in as arguments. Return the difference.
+ */
+
+
+/*
+  Create a function that divides two numbers
+  passed in as arguments. Return the quotient.
+ */
+
+
+
+/*
+  Create a function that accepts three arguments.
+    1. First number
+    2. Second number
+    3. A function that performs an operation on them
+
+  Return the value of the operation.
+ */
+
