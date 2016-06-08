4120f42e198747f7715554485f17deaae37025db
Eric Denton <eric@iamericanartist.com>
2016-06-07 19:54:29 -0500

    Completed exercise! Calculate away Has opportunity to be more DRY...

--- calculator.js
+++ calculator.js
@@ -1,101 +1,81 @@
+// I know this could be more DRY... 
+// Just wanted to get it working and may come back later clean up.
 
-// var addButtonEl = document.getElementById("addButton");             // Reference to the "+" button element in the DOM
-// var subtractButtonEl = document.getElementById("subtractButton");   // Reference to the "-" button element in the DOM
-// var multiplyButtonEl = document.getElementById("multiplyButton");   // Reference to the "*" button element in the DOM
-// var divideButtonEl = document.getElementById("divideButton");       // Reference to the "/" button element in the DOM
-// var buttonReset = document.getElementById("buttonReset");           // Reference to the button Reset element in the DOM
-// var resultsDiv = document.getElementById("results");
+//////////////////Button Locations for Listeners mostly
+var addButtonEl = document.getElementById("addButton");             // Reference to the "+" button element in the DOM
+var subtractButtonEl = document.getElementById("subtractButton");   // Reference to the "-" button element in the DOM
+var multiplyButtonEl = document.getElementById("multiplyButton");   // Reference to the "*" button element in the DOM
+var divideButtonEl = document.getElementById("divideButton");       // Reference to the "/" button element in the DOM
 
-
-
-// function additionFunction (input 1, input2) {
-//   var added = input1 + input2;
-//   console.log("input1 + input2 =", added );
-//   return added;
-// }
-// function subtractionFunction (input 1, input2) {
-//   var subtracted = input1 - input2;
-//   console.log("input1 - input2 =", subtracted);
-//   return subtracted;
-// }
-// function multiplicationFunction (input 1, input2) {
-//   var multiplied = input1 * input2;
-//   console.log("input1 * input2 =", multiplied);
-//   return multiplied;
-// }
-// function divisionFunction (input 1, input2) {
-//   var divided = input1 / input2;
-//   console.log("input1 / input2 =", divided);
-//   return divided;
-// }
+var buttonResetEl = document.getElementById("buttonResetEl");           // Reference to the button Reset element in the DOM
+var resultsDiv = document.getElementById("resultsDiv");
 
 
+// Create a function that adds two numbers passed in as arguments. Return the sum.
+function additionFunction () {
+  var input1 = parseInt(document.getElementById("userInput1").value);
+  var input2 = parseInt(document.getElementById("userInput2").value);
+  var added = input1 + input2;
+  console.log("User Input1 = ", input1);
+  console.log("User Input2 = ", input2);
+  console.log("input1 + input2 =", added );
+  resultsDiv.innerHTML = `${added}`
+  return added;
+}
+// Create a function that subtracts two numbers passed in as arguments. Return the difference.
+function subtractionFunction () {
+  var input1 = parseInt(document.getElementById("userInput1").value);
+  var input2 = parseInt(document.getElementById("userInput2").value);
+  var subtracted = input1 - input2;
+  console.log("User Input1 = ", input1);
+  console.log("User Input2 = ", input2);
+  console.log("input1 - input2 =", subtracted);
+  resultsDiv.innerHTML = `${subtracted}`
+  return subtracted;
+}
+// Create a function that multiplies two numbers passed in as arguments. Return the product.
+function multiplicationFunction (input1, input2) {
+  var input1 = parseInt(document.getElementById("userInput1").value);
+  var input2 = parseInt(document.getElementById("userInput2").value);
+  var multiplied = input1 * input2;
+  console.log("User Input1 = ", input1);
+  console.log("User Input2 = ", input2);
+  console.log("input1 * input2 =", multiplied);
+  resultsDiv.innerHTML = `${multiplied}`
+  return multiplied;
+}
+// Create a function that divides two numbers passed in as arguments. Return the quotient. 
+function divisionFunction (input1, input2) {
+  var input1 = parseInt(document.getElementById("userInput1").value);
+  var input2 = parseInt(document.getElementById("userInput2").value);
+  var divided = input1 / input2;
+  console.log("User Input1 = ", input1);
+  console.log("User Input2 = ", input2);
+  console.log("input1 / input2 =", divided);
+  resultsDiv.innerHTML = `${divided}`
+  return divided;
+}
 
+  // Create a function that accepts three arguments.
+  //   1. First number
+  //   2. Second number
+  //   3. A function that performs an operation on them
+
+  // Return the value of the operation.
+
+addButtonEl.addEventListener("click", additionFunction);
+subtractButtonEl.addEventListener("click", subtractionFunction);
+multiplyButtonEl.addEventListener("click", multiplicationFunction);
+divideButtonEl.addEventListener("click", divisionFunction);
+
+
+/////////////Cleanup Function (need to add for function scopes above to work)
+// buttonResetEl.addEventListener("click", cleanUp);
 // function cleanUp() {
-//   console.log("Clear Clicker", event );
-//   document.getElementById("userInput").value = "";
-//   document.getElementById("userInput").value = "";
-  
-//   resultsDiv.innerHTML = "Your Conversion Results";
+//   // console.log("Clear Clicker", event);
+//   // document.getElementById("userInput1").value = ""; //same as below 
+//   input1.value = "";
+//   // document.getElementById("userInput2").value = ""; //same as below 
+//   input2.value = "";
+//   resultsDiv.innerHTML = "Results=";
 // }
-
-// // addButtonEl.addEventListener("click", function (event) {
-// //   console.log("event",event);
-// // });
-
-// document.getElementById("addButton").addEventListener("click", function (event) {
-//   console.log("event",event);
-// });
-// subtractButtonEl.addEventListener("click", subtractionFunction);
-// multiplyButtonEl.addEventListener("click", );
-// divideButtonEl.addEventListener("click", );
-// buttonReset.addEventListener("click", cleanUp);
-document.getElementById("buttonReset").addEventListener("click", 
-  function (event) {
-    console.log("event",event);
-  }
-);
-
-document.getElementById("buttonReset").addEventListener("click", function(event) {
-    console.log("event", event);
-    // songDomEl.innerHTML += `${event.target.innerHTML}`
-  });
-
-console.log("asdfasdf");
-alert("hellooooooo!");
-// window.addEventListener("keypress", enterKeyPress); 
-
-/*
-  Create a function that multiplies two numbers
-  passed in as arguments. Return the product.
- */
-
-
-/*
-  Create a function that adds two numbers
-  passed in as arguments. Return the sum.
- */
-
-
-/*
-  Create a function that subtracts two numbers
-  passed in as arguments. Return the difference.
- */
-
-
-/*
-  Create a function that divides two numbers
-  passed in as arguments. Return the quotient.
- */
-
-
-
-/*
-  Create a function that accepts three arguments.
-    1. First number
-    2. Second number
-    3. A function that performs an operation on them
-
-  Return the value of the operation.
- */
-
